54c474401ad251790a2a2c28d21e7e833fa5a139
R1tschY <r1tschy@posteo.de>
2020-05-30 00:48:29 +0200

    Better debug representation for keys

--- lib/src/key.rs
+++ lib/src/key.rs
@@ -1,42 +1,109 @@
-use std::any::TypeId;
+use std::any::{type_name, TypeId};
 use std::fmt;
-use std::fmt::Debug;
 use std::hash::{Hash, Hasher};
 use std::marker::PhantomData;
 use std::sync::Arc;
 
 use crate::bind::annotation::BindAnnotation;
+use std::ops::Deref;
 
 // TODO: cache hash
 
 /// Untyped binding key consisting of id of type to inject and an optional annotation.
 ///
 /// Corresponds to definition at the injection point.
-#[derive(Hash, Eq, PartialEq, Clone, Debug)]
-pub struct Key(TypeId, Option<AnnotationHolder>);
+#[derive(Clone)]
+pub struct Key {
+    type_id: TypeId,
+    type_name: &'static str,
+    annotation: Option<AnnotationHolder>,
+}
 
 /// Typed version of `Key`.
-#[derive(Hash, Eq, PartialEq, Clone, Debug)]
+#[derive(Hash, Eq, PartialEq, Clone)]
 pub struct TypedKey<T: ?Sized + 'static>(Key, PhantomData<T>);
 
 #[derive(Clone)]
 struct AnnotationHolder(Arc<dyn BindAnnotation>, String);
 
 impl Key {
+    fn internal_new(
+        type_id: TypeId,
+        type_name: &'static str,
+        annotation: Option<AnnotationHolder>,
+    ) -> Self {
+        Self {
+            type_id,
+            type_name,
+            annotation,
+        }
+    }
+
     /// Create from only a type.
     #[inline]
     pub fn new<T: ?Sized + 'static>() -> Self {
-        Self(TypeId::of::<T>(), None)
+        Self::internal_new(TypeId::of::<T>(), type_name::<T>(), None)
     }
 
     /// Create from type and annotation.
     pub fn new_with_annotation<T: ?Sized + 'static, U: BindAnnotation>(annotation: U) -> Self {
-        Self(TypeId::of::<T>(), Some(AnnotationHolder::new(annotation)))
+        Self::internal_new(
+            TypeId::of::<T>(),
+            type_name::<T>(),
+            Some(AnnotationHolder::new(annotation)),
+        )
     }
 
     /// Create from type and annotation.
     pub fn with_annotation<U: BindAnnotation>(&self, annotation: U) -> Self {
-        Self(self.0, Some(AnnotationHolder::new(annotation)))
+        Self::internal_new(
+            self.type_id,
+            self.type_name,
+            Some(AnnotationHolder::new(annotation)),
+        )
+    }
+
+    /// Type id
+    pub fn type_id(&self) -> TypeId {
+        self.type_id
+    }
+
+    /// Type name from `std::any::type_name`
+    pub fn type_name(&self) -> &'static str {
+        self.type_name
+    }
+
+    /// Type id of annotation type
+    pub fn annotation_type_id(&self) -> Option<TypeId> {
+        self.annotation.as_ref().map(|a| a.0.type_id())
+    }
+
+    /// annotation value formatted using `Debug` trait
+    pub fn annotation_debug(&self) -> Option<&str> {
+        self.annotation.as_ref().map(|a| &a.1 as &str)
+    }
+}
+
+impl Hash for Key {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.type_id.hash(state);
+        self.annotation.hash(state);
+    }
+}
+
+impl PartialEq for Key {
+    fn eq(&self, other: &Self) -> bool {
+        self.type_id == other.type_id && self.annotation == other.annotation
+    }
+}
+
+impl Eq for Key {}
+
+impl fmt::Debug for Key {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple(&format!("Key<{}>", self.type_name))
+            .field(&self.annotation)
+            .finish()
     }
 }
 
@@ -57,6 +124,22 @@ impl<T: ?Sized + 'static> TypedKey<T> {
 impl<T: ?Sized + 'static> Default for TypedKey<T> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl<T: ?Sized + 'static> fmt::Debug for TypedKey<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple(&format!("TypedKey<{}>", self.type_name()))
+            .field(&self.annotation_debug())
+            .finish()
+    }
+}
+
+impl<T: ?Sized + 'static> Deref for TypedKey<T> {
+    type Target = Key;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
@@ -89,7 +172,7 @@ impl PartialEq for AnnotationHolder {
     }
 }
 
-impl Debug for AnnotationHolder {
+impl fmt::Debug for AnnotationHolder {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(&self.1)
     }
